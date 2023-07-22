531c85011578ab6696ec0d6bec1cf6ae201535f2
Mari-Gogolashvili <133369792+Mari-Gogolashvili@users.noreply.github.com>
2023-07-22 23:46:23 +0400

    .

--- main.js
+++ main.js
@@ -1,97 +1,90 @@
-const carousel=document.querySelector(".carousel");
+const carousel = document.querySelector(".carousel");
 const firstImg = carousel.querySelectorAll("img")[0];
 const arrowIcons = document.querySelectorAll(".wrapper i");
-
-
 
 carousel.addEventListener("mousedown", dragStart);
 carousel.addEventListener("touchstart", dragStart);
 
-carousel.addEventListener("mousemove",dragging);
-carousel.addEventListener("touchmove",dragging);
+carousel.addEventListener("mousemove", dragging);
+carousel.addEventListener("touchmove", dragging);
 
-carousel.addEventListener("mouseup",dragStop);
-carousel.addEventListener("mouseleave",dragStop);
-carousel.addEventListener("touchend",dragStop);
+carousel.addEventListener("mouseup", dragStop);
+carousel.addEventListener("mouseleave", dragStop);
+carousel.addEventListener("touchend", dragStop);
 
+let isDragStart = false,
+  isDragging = false,
+  prevPageX,
+  prevScrollLeft,
+  positionDiff;
 
-let isDragStart=false, isDragging=false,prevPageX , prevScrollLeft , positionDiff;
-
-function dragStart(e){
-    isDragStart=true;
-    prevPageX=e.pageX  || e.touches[0].pageX ;
-    prevScrollLeft=carousel.scrollLeft;
+function dragStart(e) {
+  isDragStart = true;
+  prevPageX = e.pageX || e.touches[0].pageX;
+  prevScrollLeft = carousel.scrollLeft;
 }
 
-function dragging(e){
-    if(!isDragStart) return;
-    e.preventDefault();
-    isDragging=true;
-    carousel.classList.add("dragging");
-    positionDiff=(e.pageX || e.touches[0].pageX) - prevPageX;
-    carousel.scrollLeft=prevScrollLeft - positionDiff;
-    showHideIcons();
-    
+function dragging(e) {
+  if (!isDragStart) return;
+  e.preventDefault();
+  isDragging = true;
+  carousel.classList.add("dragging");
+  positionDiff = (e.pageX || e.touches[0].pageX) - prevPageX;
+  carousel.scrollLeft = prevScrollLeft - positionDiff;
+  showHideIcons();
 }
 
-function dragStop(){
-    isDragStart=false;
-    carousel.classList.remove("dragging");
-    if(!isDragging) return;
-    isDragging=false;
+function dragStop() {
+  isDragStart = false;
+  carousel.classList.remove("dragging");
+  if (!isDragging) return;
+  isDragging = false;
 
-    autoSlide();
+  autoSlide();
 }
 
+function autoSlide() {
+  if (carousel.scrollLeft == carousel.scrollWidth - carousel.clientWidth) return;
 
-function autoSlide(){
-    if(carousel.scrollLeft == (carousel.scrollWidth - carousel.clientWidth )) return;
+  positionDiff = Math.abs(positionDiff);
+  let firstImgWidth = firstImg.clientWidth + 14;
+  let valDifference = firstImgWidth - positionDiff;
 
-
-    positionDiff = Math.abs (positionDiff);
-    let firstImgWidth = firstImg.clientWidth + 14 ;
-    let valDifference = firstImgWidth - positionDiff;
-
-    if(carousel.scrollLeft > prevScrollLeft){
-        return carousel.scrollLeft += positionDiff / 3 ? valDifference : -positionDiff;
-     }
-    carousel.scrollLeft -= positionDiff / 3 ? valDifference : -positionDiff;
-
+  if (carousel.scrollLeft > prevScrollLeft) {
+    return (carousel.scrollLeft += positionDiff / 3 ? valDifference : -positionDiff);
+  }
+  carousel.scrollLeft -= positionDiff / 3 ? valDifference : -positionDiff;
 }
 
+function showHideIcons() {
+  let scrollWidth = carousel.scrollWidth - carousel.clientWidth; // მაქსიმალური სიგანე
+  if (carousel.scrollLeft == 0) {
+    arrowIcons[0].style.display = "none";
+  } else {
+    arrowIcons[0].style.display = "block";
+  }
 
-function showHideIcons(){
-    let scrollWidth = carousel.scrollWidth - carousel.clientWidth // მაქსიმალური სიგანე 
-    if(carousel.scrollLeft == 0){
-        arrowIcons[0].style.display = "none";
+  if (carousel.scrollLeft == scrollWidth) {
+    arrowIcons[1].style.display = "none";
+  } else {
+    arrowIcons[1].style.display = "block";
+  }
+}
+
+arrowIcons.forEach((icon) => {
+  icon.addEventListener("click", () => {
+    let firstImgWidth = firstImg.clientWidth + 14;
+    if (icon.id === "left") {
+      carousel.scrollLeft = carousel.scrollLeft - firstImgWidth;
     } else {
-        arrowIcons[0].style.display = "block";
+      carousel.scrollLeft = carousel.scrollLeft + firstImgWidth;
     }
-     
-    if (carousel.scrollLeft == scrollWidth){
-        arrowIcons[1].style.display = "none";
-    } else {
-        arrowIcons[1].style.display = "block";
-    }
-
-
-}
-
-
-arrowIcons.forEach(icon => {
-    icon.addEventListener("click" , () => {
-        let firstImgWidth = firstImg.clientWidth + 14 ;
-        if(icon.id==="left"){
-            carousel.scrollLeft = carousel.scrollLeft -firstImgWidth;
-        }else {
-            carousel.scrollLeft = carousel.scrollLeft +firstImgWidth;
-   
-        }
-        setTimeout(() => showHideIcons(),20 );
-    });
+    setTimeout(() => showHideIcons(), 20);
+  });
 });
 
 
-
-
-
+//gsghs
+//!askjdak
+//*sjbdkwjd
+//?sjqqjkq
--- recipes/Results/results.js
+++ recipes/Results/results.js
@@ -1,18 +1,20 @@
+/** @format */
+
 const urlSearchParams = new URLSearchParams(window.location.search);
 const params = Object.fromEntries(urlSearchParams.entries());
-const Mealid=params.id;
+const Mealid = params.id;
 console.log(Mealid);
 // const recipeResult = document.querySelector(".recipe-search-result");
-const first=document.querySelector(".first");
-const second=document.querySelector(".second");
-const third=document.querySelector(".third")
+const first = document.querySelector(".first");
+const second = document.querySelector(".second");
+const third = document.querySelector(".third");
 
 fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${Mealid}`)
-  .then((response)=> response.json())
-  .then((data) =>{
+  .then((response) => response.json())
+  .then((data) => {
     console.log(data);
-    const firstDiv = document.createElement('div');
-    firstDiv.innerHTML=`
+    const firstDiv = document.createElement("div");
+    firstDiv.innerHTML = `
                     <div class="image">
                         <img src="${data.meals[0].strMealThumb}" alt="${data.meals[0].strMeal}">
                     </div>
@@ -28,22 +30,21 @@ fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${Mealid}`)
                     </div>
     `;
 
-    const ingredientList = document.createElement('ul');
+    const ingredientList = document.createElement("ul");
 
-    for(let i=1 ; i<21 ; i++){
-        const ingredient = data.meals[0][`strIngredient${i}`];
-        const measure = data.meals[0][`strMeasure${i}`];
+    for (let i = 1; i < 21; i++) {
+      const ingredient = data.meals[0][`strIngredient${i}`];
+      const measure = data.meals[0][`strMeasure${i}`];
 
-        if(ingredient && measure !=='' || ingredient && measure !== null){
-            const li = document.createElement("li");
-            li.innerHTML=`<span>${ingredient}:</span>${measure}</span>`;
-            ingredientList.appendChild(li);
-        }
+      if ((ingredient && measure !== "") || (ingredient && measure !== null)) {
+        const li = document.createElement("li");
+        li.innerHTML = `<span>${ingredient}:</span>${measure}</span>`;
+        ingredientList.appendChild(li);
+      }
     }
 
-
-    const instruction = document.createElement('div');
-    instruction.innerHTML=`
+    const instruction = document.createElement("div");
+    instruction.innerHTML = `
         <div class="meal-instruction">
             <p>${data.meals[0].strInstructions}</p>
         </div>
@@ -53,14 +54,8 @@ fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${Mealid}`)
     second.appendChild(ingredientList);
     third.appendChild(instruction);
 
-
-
     const videoBtn = firstDiv.querySelector(".video-button");
-    videoBtn.addEventListener("click" , ()=>{
-        window.open(data.meals[0].strYoutube, "_blank");
-    }
-    );
-
-})
-
-
+    videoBtn.addEventListener("click", () => {
+      window.open(data.meals[0].strYoutube, "_blank");
+    });
+  });
